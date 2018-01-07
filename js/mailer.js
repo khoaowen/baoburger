fc658ea882b571dd6e1d1890d3a9f7f277546e8b
khoa <khoa@mail>
2018-01-07 19:21:58 +0100

    Update site address Add send mail action php Add jquery & parsley for client validation

---
+++ js/mailer.js
@@ -0,0 +1,56 @@
+$(function() {
+    // Get the form.
+    var form = $('#ajax-contact-form');
+
+    // Get the messages div.
+    var formMessages = $('#form-messages');
+
+    $(form).parsley().on('field:validated', function() {
+        var ok = $('.parsley-error').length === 0;
+        $('.bs-callout-info').toggleClass('hidden', !ok);
+        $('.bs-callout-warning').toggleClass('hidden', ok);
+    })
+    
+    // Set up an event listener for the contact form.
+    $(form).submit(function(event) {
+        // Stop the browser from submitting the form.
+        event.preventDefault();
+
+        // Serialize the form data.
+        var formData = $(form).serialize();
+        
+        // Submit the form using AJAX.
+        $.ajax({
+            type: 'POST',
+            url: $(form).attr('action'),
+            data: formData
+        })
+        .done(function(response) {
+            // Make sure that the formMessages div has the 'success' class.
+            $(formMessages).removeClass('w3-red');
+            $(formMessages).addClass('w3-green');
+
+            // Set the message text.
+            $(formMessages).text(response);
+
+            // Clear the form.
+            $('#name').val('');
+            $('#email').val('');
+            $('#message').val('');
+        })
+        .fail(function(data) {
+            // Make sure that the formMessages div has the 'error' class.
+            $(formMessages).removeClass('w3-green');
+            $(formMessages).addClass('w3-red');
+
+            // Set the message text.
+            if (data.responseText !== '') {
+                $(formMessages).text(data.responseText);
+            } else {
+                $(formMessages).text('Oops! An error occured and your message could not be sent.');
+            }
+        });
+        
+    });
+    
+});
